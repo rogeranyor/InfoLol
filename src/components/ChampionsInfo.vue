dd170ed1f3160c1bb2536e910bf850171fd32c93
rogeranyor <ranorgra18@alumnes.ub.edu>
2022-05-15 14:37:06 +0200

    Champions info terminado

--- src/components/ChampionsInfo.vue
+++ src/components/ChampionsInfo.vue
@@ -1,69 +1,547 @@
-<template>
-<div class="container">
-  <div class="row row-cols-3" style="margin:2%">
-    <div class="col">
-      <img src="../assets/akali.png" class="img-fluid" >
-    </div>
-    <div class="col"><br><br><br>
-        <div class="badge bg-primary text-wrap" style="width:auto" >
-            <h3>Champion Name</h3>
+<template >
+  <div class="container" style="background-color: darkgray;">
+    <div class="row row-cols-3" style="margin: 2%">
+      <div class="col">
+        <img src="../assets/akali.png" class="img-fluid" />
+      </div>
+      <div class="col">
+        <br /><br /><br />
+        <div class="badge bg-primary text-wrap" style="width: auto;color:white ;">
+          <h3>Akali</h3>
         </div>
+      </div>
+      <div class="col">
+        <div class="circle">
+          <h3 style="color: blue">Winrate</h3>
+        </div>
+      </div>
     </div>
-    <div class="col">
-            <div class="circle">
-                <h3 style="color:blue">Winrate</h3>
-            </div>
+
+    <div class="row" style="margin-top: 2%">
+      <div class="col" style="width: fit-content; margin: 2%">
+        <div
+          class="badge bg-primary text-wrap"
+          style="width: fit-content; text-align: center; margin: 2% ;color:white"
+        >
+          <h3>Runes</h3>
+        </div>
+        <table align="center">
+          <tbody align="center">
+            <tr>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="48"
+                      width="48"
+                      style="1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="48"
+                      width="48"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="48"
+                      width="48"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="48"
+                      width="48"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="bigBorderBottom text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="bigBorderBottom text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="bigBorderBottom text-center">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="36"
+                      width="36"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="bigBorderBottom">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="bigBorderBottom">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="bigBorderBottom">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="1"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="1"
+                    />
+                  </div>
+                </div>
+              </td>
+              <td colspan="3" class="small-vert-padding">
+                <div class="img-align-block">
+                  <div>
+                    <img
+                      src="/static/img/akali.6ea05c5.png"
+                      height="24"
+                      width="24"
+                      style="opacity: 0.25"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col" style="width: fit-content; margin: 2%">
+        <div
+          class="badge bg-primary text-wrap"
+          style="margin: 2%; background-color: blue;color:white"
+        >
+          <h3>Build</h3>
+        </div>
+        <div class="row">
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+          <img
+            data-original="/images/item/sorcerers-shoes-64x.png"
+            alt=""
+            width="64"
+            height="64"
+            src="/static/img/akali.6ea05c5.png"
+            style="margin: 2%"
+          />
+        </div>
+        <div class="content-section_content skill-priority">
+          <div
+          class="badge bg-primary text-wrap"
+          style="margin: 2%; background-color: blue;color:white">
+          <h3>Skill Priority</h3>
+          </div>
+          <table align="center">
+            <tbody>
+              <tr>
+              <td>
+                <div class="champion-skill-with-label" style="margin: 2%">
+                  <img
+                    src="https://static.u.gg/assets/lol/riot_static/12.9.1/img/spell/AkaliQ.webp"
+                  />
+                  <div class="skill-label bottom-center">Q</div>
+                </div>
+              </td>
+              <td>
+                <div class="champion-skill-with-label" style="margin: 2%">
+                  <img
+                    src="https://static.u.gg/assets/lol/riot_static/12.9.1/img/spell/AkaliE.webp"
+                  />
+                  <div class="skill-label bottom-center">E</div>
+                </div>
+              </td>
+              <td>
+                <div class="champion-skill-with-label" style="margin: 2%">
+                  <img
+                    src="https://static.u.gg/assets/lol/riot_static/12.9.1/img/spell/AkaliW.webp"
+                  />
+                  <div class="skill-label bottom-center">W</div>
+                </div>
+              </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
-
-  <div class="row row-cols-2" style="margin-top:2%;">
-    <div class="col">
-        <div class="badge bg-primary text-wrap" style="width:auto;margin:2%">
-            <h3>Runes</h3>
-        </div>
-        <div class="col">
-            <img src="../assets/runes.png" class="img-fluid">
-        </div>
-    </div>
-    <div class="col">
-        <div class="badge bg-primary text-wrap" style="width:auto;margin:2%">
-            <h3>Build</h3>
-        </div>
-      <div class="col">
-            <img src="../assets/build.png" class="img-fluid">
-        </div>
-        <div class="badge bg-primary text-wrap" style="width:auto;margin:2%">
-            <h3>Ability Order</h3>
-        </div>
-      <div class="col">
-            <img src="../assets/hability.png" class="img-fluid">
-        </div>
-    </div>
-  </div>
-</div>
-
 </template>
 
 <style>
 .circle {
-    background: yellow;
-    border-radius: 200px;
-    color: white;
-    height: 200px;
-    font-weight: bold;
-    width: 200px;
-    display: table;
-    margin: 20px auto;
+  background: yellow;
+  border-radius: 200px;
+  color: white;
+  height: 200px;
+  font-weight: bold;
+  width: 200px;
+  display: table;
+  margin: 20px auto;
 }
 .circle h3 {
-    vertical-align: middle;
-    display: table-cell;
+  vertical-align: middle;
+  display: table-cell;
 }
 </style>
 
 <script>
 export default {
   props: {
-    championName: null}
+    championName: null
+  }
 }
 </script>
