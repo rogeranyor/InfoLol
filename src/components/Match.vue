4e47a2d46be90fbce7690b359a8da3ebc2b5ca59
Topiksit0 <marcospalominovivar@gmail.com>
2022-05-15 20:49:55 +0200

    ROGER AÑOR MARICON

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,25 +1,5 @@
 <template>
 <div>
-  <nav class="navbar navbar-expand-lg navbar-light py-3" style="background-color:dodgerblue;">
-      <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                  <a class="nav-link" href="//codeply.com" style="color: gold; padding-left:30px;">Who are we?</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#" style="color: gold;">Contact Us</a>
-              </li>
-          </ul>
-      </div>
-      <div class="mx-auto order-0">
-          <a class="navbar-brand mx-auto" href="#" style="font-size: 80px;color: gold;">InfoLoL</a>
-      </div>
-      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-          <ul class="navbar-nav ml-auto">
-          </ul>
-      </div>
-  </nav>
-
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-9">
